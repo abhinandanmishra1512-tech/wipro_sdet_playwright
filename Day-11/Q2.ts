f97477fe787a55462b156d83abdd53a93bf7e699
abhinandanmishra1512 <abhinandanmishra1512@gmail.com>
2026-05-13 14:45:13 +0530

    Practise Questions

---
+++ Day-11/Q2.ts
@@ -0,0 +1,13 @@
+enum Role {
+  Admin = "Admin",
+  Editor = "Editor",
+  Guest = "Guest"
+}
+
+const PermissionMap: Record<Role, boolean> = {
+  [Role.Admin]: true,
+  [Role.Editor]: true,
+  [Role.Guest]: false,
+};
+
+console.log(PermissionMap);
---
+++ Day-11/Q7.ts
@@ -0,0 +1,24 @@
+type AllEvents = 'click' | 'dbclick' | 'submit' | 'reset' | 'keypress';
+
+
+type MouseEvents = Extract<AllEvents, 'click' | 'dbclick'>;
+
+
+
+type NonFormEvents = Exclude<AllEvents, 'submit' | 'reset'>;
+
+
+function handleMouseEvent(event: MouseEvents): void {
+  console.log("Mouse event triggered:", event);
+}
+
+function handleNonFormEvent(event: NonFormEvents): void {
+  console.log("Non-form event triggered:", event);
+}
+
+handleMouseEvent('click');      
+handleMouseEvent('dbclick');     
+
+
+handleNonFormEvent('click');     
+handleNonFormEvent('keypress');  
